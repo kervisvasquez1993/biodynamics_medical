399b829c583c24bf91614f9b5424acbdadb133f5
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-11-13 10:55:00 -0400

    agregamos item de categoria para el filtro

--- functions.php
+++ functions.php
@@ -44,7 +44,8 @@ add_action( 'pre_get_posts', 'add_custom_pt' );
 // Cargar los estilos y los script de la pagina
 
 
-    function style(){
+    function style()
+    {
         wp_enqueue_style('raleweay','https://fonts.googleapis.com/css?family=Raleway&display=swap',array(), '1.0.0'.'all');
         wp_enqueue_style('Oxygen','https://fonts.googleapis.com/css?family=Oxygen&display=swap',array(),'1.0.0','all');
         wp_enqueue_style('quicksand','https://fonts.googleapis.com/css?family=Quicksand&display=swap',array(),'1.0.0','all');
@@ -58,24 +59,22 @@ add_action( 'pre_get_posts', 'add_custom_pt' );
         // wp_enqueue_style('fontawesome',get_template_directory_uri().'/css/font-awesome.css',array(),'4','all');
         wp_enqueue_style('style',get_stylesheet_uri());
         //wp_enqueue_script('sc6',get_template_directory_uri().'/js/es6.js',array(),'1.0.0',true);
-
         wp_enqueue_script('jquery');
         wp_enqueue_script('popper','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array('jquery'),'4.2.3',true);
         wp_enqueue_script('bootstrap-js','https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array('popper'),'4.2.3', true);
         wp_enqueue_script('scripts', get_template_directory_uri().'/js/scripts.js',array(),'1.0.0', true);
-
     }
-
     add_action('wp_enqueue_scripts','style');
 
 
 //creacion de menu
 
     function menus(){
-        register_nav_menus( array(
+        register_nav_menus( 
+        array
+        (
             'header_menu' => __('header_menu', 'biodynamicsmedical'),
             'trauma_menu' => __('trauma_menu', 'biodynamicsmedical')
-
         ));
     }
     add_action( 'init', 'menus' );
--- taxonomy.php
+++ taxonomy.php
@@ -41,58 +41,61 @@ $args = array(
  Array ( [name] => Sistema Biolock® 3.5 [id] => 115 ) 
  Array ( [name] => Sistema Biolock® MIS [id] => 120 )
 
-Array ( [0] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 )
-        [1] => Array ( [0] => Sistema Biolock® MIS [1] => 120 ) 
-        [2] => Array ( [0] => Sistema DHS / DCS [1] => 118 ) 
-        [3] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 ) 
-        [4] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 ) 
+Array ( [0] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 ) 
+        [1] => Array ( [0] => Sistema Biolock® MIS [1] => 120 )
+        [2] => Array ( [0] => Sistema DHS / DCS [1] => 118 )
+        [3] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 )
+        [4] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 )
         [5] => Array ( [0] => Sistema Biolock® MIS [1] => 120 ) 
         [6] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 ) 
-        [7] => Array ( [0] => Sistema Biolock® MIS [1] => 120 ) 
-    )
+        [7] => Array ( [0] => Sistema Biolock® MIS [1] => 120 )
+     )
  */
 ?>
 <div class="item active responsive">
     <img src="<?php echo get_template_directory_uri();?>/img/banner-seccion-division.jpg" alt="">
 </div>
 <div class="container">
-<?php $farmaco = new WP_Query($args);  
-        if($farmaco->have_posts()) : $elemento_arr = []; 
-                         while($farmaco->have_posts()): $farmaco->the_post();
-                         $terms = wp_get_post_terms(get_the_ID(), 'categoria-producto');
-                         $name  = [$terms[0]->name , $terms[0]->term_id] ;
-                         array_push($elemento_arr, $name);
-                         ?>
-                        <?php endwhile; wp_reset_postdata();
-                        print_r($elemento_arr);
+<?php $biodynamics = new WP_Query($args);  
+        if($biodynamics->have_posts()) :
+                     $elemento_arr        = [];
+                     $array_nuevo_letra   = [];
+                     $array_nuevo_numero  = [];
+                     $i                   =  0; 
+                        while($biodynamics->have_posts()): $biodynamics->the_post();
+                              $terms = wp_get_post_terms(get_the_ID(), 'categoria-producto');
+                              $name  = [$terms[0]->name , $terms[0]->term_id];
+                              array_push($elemento_arr, $name);
+                              array_push($array_nuevo_letra,$elemento_arr[$i][0]);
+                              array_push($array_nuevo_numero,$elemento_arr[$i][1]);
+                              $entradas = array_unique($array_nuevo_letra);
+                              $numeros  = array_unique($array_nuevo_numero);                        
+                              $i++;
+                              ?>
+                <?php   endwhile; wp_reset_postdata();
                         
-                       
-                        
-                        
-                        
-                        
-                        
-                        
+                        $longitud = sizeof($entradas);
+                        $n = 0;
                         ?>
-                        <div class="flex-row row simplefilter">
-                                <?php
-                                foreach($menu_categorias as $menu):?>
-                                    
-                                    <div class="fltr-controls col-xs-2 col-sm-3 col-lg-3">
-                                      <div class="thumbanil">
+                       <div class="flex-row row simplefilter">
+                            <?php
+                              for($n; $n < $longitud ; $n++):?>
+                                <div class="fltr-controls col-xs-2 col-sm-3 col-lg-3" data-filter="<?php echo $numeros[$n];?>">
+                                    <div class="thumbanil">
                                         <div class="caption product-description NewsCycle">
-                                             <h4> 
-                                                <?php // print_r($menu);?>
+                                            <h4> 
+                                              <?php echo $entradas[$n]; ?>
                                             </h4>
                                         </div>
-                                     </div>
                                     </div>
-                                <?php endforeach;?>
+                                </div>
+                            <?php
+                                endfor;
+                            ?>
                         </div>
-
                         <!--comenzamos la seccion de post de cada productos -->
-                        <div class="flex-row row">
-                             <?php while($farmaco->have_posts()): $farmaco->the_post();
+                        <div class="flex-row row multifilter">
+                             <?php while($biodynamics->have_posts()): $biodynamics->the_post();
                               $terms = wp_get_post_terms(get_the_ID(), 'categoria-producto');
                               $id_tax = $terms[0];
                               $imagen_categoria_producto = get_field('img_taxonomia',$id_tax);
@@ -113,7 +116,7 @@ Array ( [0] => Array ( [0] => Sistema Biolock® 3.5 [1] => 115 )
                                         </div>
                                     </div>
                                 </div>   
-                            </div>
+                              </div>
 			                 <?php endwhile; wp_reset_postdata();?>
 			           
  <?php endif;?>
